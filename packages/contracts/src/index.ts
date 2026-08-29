e17664059b88a73c9b95a1e23abdd6ecd31baffb
Thiago <thicosta1432@gmail.com>
2026-08-29 17:54:01 -0300

    feat: add shared mandate contracts

---
+++ packages/contracts/src/index.ts
@@ -0,0 +1,6 @@
+export * from './errors.js';
+export * from './mandate.js';
+export * from './merchant.js';
+export * from './payment.js';
+export * from './primitives.js';
+export * from './proofs.js';
